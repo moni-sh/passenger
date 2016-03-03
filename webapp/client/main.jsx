b36f2971940bbae86135a6a1435085a842ecec92
yonatan braude <yonatan.py@gmail.com>
2016-03-03 16:00:46 +0200

    lines with numbers :)

--- webapp/client/main.jsx
+++ webapp/client/main.jsx
@@ -1,7 +1,25 @@
 WelcomeComponent = React.createClass({
     render() {
-        return <div>
-        <h1>Hello, {this.props.name}</h1>
+        return <div className="monish">
+            {this.state.lines.map( line => {return <Line {...line}></Line>})}
         </div>
+    },
+    getInitialState() {
+        return {
+            lines: [
+                {number: 4},
+                {number: 5},
+                {number: 16},
+                {number: 51}
+            ]
+        }
+    }
+});
+
+Line = React.createClass({
+    render() {
+        return (<div className="line">
+            {this.props.number}
+        </div>);
     }
 });
